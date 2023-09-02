5fbf16737a3393c371fcb7ee32d6a2777ecbdf99
Christian <purin213@hotmail.com>
2023-09-02 20:46:22 +0900

    convert md to html

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import markdown
 html =  markdown.markdown(sys.argv[1])
+with open(sys.argv[1], 'r') as md:
+    mdtext = md.read()
+    html = markdown.markdown(mdtext)
+    with open(sys.argv[2], 'w') as output:
+        output.write(html)
